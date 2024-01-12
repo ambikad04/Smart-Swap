ba6a00ad35c8cce80afe81aa1a471a05d04d5f46
ambikad04 <ambikad2003@gmail.com>
2024-01-13 00:27:47 +0530

    update script,js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-const dropList = document.querySelectorAll(".drop-list select");
-
-for(let i = 0; i < dropList.length; i++) {
-    for(currency_code in country_code){
-        let optionTag = '<option value="${currency_code}">${currency_code}</option';
-        dropList[i].insertAdjacentHTML("beforeend", optionTag);
-    }
-}
+let api= 'https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD';
+const fromDropDown = document.getElementById("from-currency-select");
+const toDropDown = document.getElementById("to-currency-select");
