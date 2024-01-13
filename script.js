af8e451521c01b7dacf0ead035891efdafd1acd8
ambikad04 <ambikad2003@gmail.com>
2024-01-13 14:11:20 +0530

    update api-key.js

--- script.js
+++ script.js
@@ -17,3 +17,35 @@ currencies.forEach((currency) => {
     option.text = currency;
     toDropDown.add(option);
 });
+
+
+// Select default values
+fromDropDown.value = "USD";
+toDropDown.value = "INR";
+
+let convertCurrency = () => {
+    // Create References
+    const amount = document.querySelector("#amount").value;
+    const fromCurrency = fromDropDown.value;
+    const toCurrency = toDropDown.value;
+
+
+    // If amount input field is not empty
+    if(amount.length != 0){
+        fetch(api)
+            .then((resp) => resp.json())
+            .then((data) => {
+                let fromExchangeRate = data.conversion_rates;
+                [fromCurrency];
+                let toExchangeRate = data.conversion_rates[toCurrency];
+            });
+    } else {
+        alert("Please fill in the amount");
+    }
+};
+
+
+document
+    .querySelector("#convert-button")
+    .addEventListener("click", convertCurrency);
+    window.addEventListener("load", convertCurrency);
